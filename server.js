350127b5c260de3eaf5243f886621d9aa2add6bc
Kalaivani <kalaivani@Kalaivanis-MacBook-Air.local>
2019-01-02 23:48:01 +0530

    TodoApp created

--- server.js
+++ server.js
@@ -5,22 +5,52 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	//fs.appendFile('server.log', log + '\n');
+	next();
+});
+
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
 		pageTitle: 'Home Page',
-		welcomeMessage: 'Welcome!!!',
-		currentYear: new Date().getFullYear()
+		welcomeMessage: 'Welcome!!!'
 
 	});
 });
 
 app.get('/about', (req, res) => {
 	res.render('about.hbs', {
-		pageTitle: 'About Page',
-		currentYear: new Date().getFullYear()
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page',
+		welcomeMessage: 'Welcome!!!'
+	});
+});
+
+app.get('/bad', (req,res) => {
+	res.send({
+		errorMessage: 'Unable to handle request'
 	});
 });
 
