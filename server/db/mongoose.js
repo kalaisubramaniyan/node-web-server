b80b4ecf982053ce33aef437de27e3173955e26d
Kalaivani <kalaivani@Kalaivanis-MacBook-Air.local>
2019-01-09 23:58:07 +0530

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1/TodoApp');
+mongoose.connect('mongodb://127.0.0.1/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
